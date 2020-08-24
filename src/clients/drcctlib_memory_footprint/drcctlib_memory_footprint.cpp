193be3f436aa51ed30f0c375627c58107d9ae08f
VivekReddy98 <vivekreddykarri98@gmail.com>
2020-08-23 23:14:26 -0400

    Added a new client

---
+++ src/clients/drcctlib_memory_footprint/drcctlib_memory_footprint.cpp
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2020 Xuhpclab. All rights reserved.
+ *  Licensed under the MIT License.
+ *  See LICENSE file for more information.
+ */
+
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include <iterator>
+#include <unistd.h>
+#include <vector>
+#include <map>
+
+#include <sys/resource.h>
+#include <sys/mman.h>
+
+#include "dr_api.h"
+#include "drmgr.h"
+#include "drsyms.h"
+#include "drreg.h"
+#include "drutil.h"
+#include "drcctlib.h"
+
+using namespace std;
+
+#define DRCCTLIB_PRINTF(format, args...) \
+    DRCCTLIB_PRINTF_TEMPLATE("memory_footprint", format, ##args)
+#define DRCCTLIB_EXIT_PROCESS(format, args...) \
+    DRCCTLIB_CLIENT_EXIT_PROCESS_TEMPLATE("memory_footprint", format, ##args)
+
+
+static inline void
+InstrumentPerInsCache(void *drcontext, context_handle_t ctxt_hndl, int32_t mem_ref_num,
+                      mem_ref_msg_t *mem_ref_start, void *data)
+{
+}
+
+static inline void
+InstrumentPerBBCache(void *drcontext, context_handle_t ctxt_hndl, int32_t slot_num,
+                     int32_t mem_ref_num, mem_ref_msg_t *mem_ref_start, void **data)
+{
+}
+
+static void
+ClientInit(int argc, const char *argv[])
+{
+}
+
+static void
+ClientExit(void)
+{
+    drcctlib_exit();
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DR_EXPORT void
+dr_client_main(client_id_t id, int argc, const char *argv[])
+{
+    dr_set_client_name("DynamoRIO Client 'drcctlib_memory_footprint'",
+                       "http://dynamorio.org/issues");
+    ClientInit(argc, argv);
+
+    drcctlib_init_ex(DRCCTLIB_FILTER_MEM_ACCESS_INSTR, INVALID_FILE, NULL, NULL,
+                     InstrumentPerBBCache, DRCCTLIB_CACHE_MODE);
+    dr_register_exit_event(ClientExit);
+}
+
+#ifdef __cplusplus
+}
+#endif
