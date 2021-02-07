5c7e9336c4b20653bbd01a0b75a49294bfff9f18
czhong4 <czhong4@ncsu.edu>
2021-02-06 21:35:27 -0500

    add deadlock examples

---
+++ test_apps/src/goroutines/circular_waiting/multi_goroutines.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type ms struct {
+	m1 sync.Mutex
+	m2 sync.Mutex
+	m3 sync.Mutex
+	m4 sync.Mutex
+}
+
+func main() {
+	a := 0
+	var m ms
+	go func() {
+		m.m1.Lock()
+		m.m4.Lock()
+		a++
+		fmt.Println(a)
+		m.m4.Unlock()
+		m.m1.Unlock()
+	}()
+	go func() {
+		m.m2.Lock()
+		m.m1.Lock()
+		a++
+		fmt.Println(a)
+		m.m1.Unlock()
+		m.m2.Unlock()
+	}()
+	go func() {
+		m.m3.Lock()
+		m.m2.Lock()
+		a++
+		fmt.Println(a)
+		m.m2.Unlock()
+		m.m3.Unlock()
+	}()
+	go func() {
+		m.m4.Lock()
+		m.m3.Lock()
+		a++
+		fmt.Println(a)
+		m.m3.Unlock()
+		m.m4.Unlock()
+	}()
+	time.Sleep(time.Second)
+}
